ce89a0894d7e3f4f3170b722c56bc35dd6eadcb6
Dmitriy Mysyagin <dmitry.mysyagin@yandex.ru>
2022-07-12 22:15:18 +0300

    second app vue

--- app.js
+++
@@ -1,9 +0,0 @@
-Vue.createApp({
-  data() {
-    return { 
-      counter: 0,
-      title: "Счетчик" 
-    }
-  }
-}
-).mount('#app')
---
+++ first app/app.js
@@ -0,0 +1,10 @@
+Vue.createApp({
+  data() {
+    return { 
+      counter: 0,
+      title: "Счетчик",
+      myPlaceHolder: "Введите название задачи"
+    }
+  }
+}
+).mount('#appp')
